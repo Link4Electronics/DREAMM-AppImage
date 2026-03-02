71b5420472fc5ba98b6e6a94e59917160a336e55
Link4Electronics <link4electronics@gmail.com>
2026-03-01 21:35:55 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,16 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/dreamm
+quick-sharun ./AppDir/bin/dreamm \
+/usr/lib/alsa-lib/libasound_module_pcm_a52.so \
+/usr/lib/alsa-lib/libasound_module_pcm_alsa_dsp.so \
+/usr/lib/alsa-lib/libasound_module_pcm_jack.so \
+/usr/lib/alsa-lib/libasound_module_pcm_oss.so \
+/usr/lib/alsa-lib/libasound_module_pcm_pipewire.so \
+/usr/lib/alsa-lib/libasound_module_pcm_speex.so \
+/usr/lib/alsa-lib/libasound_module_pcm_upmix.so \
+/usr/lib/alsa-lib/libasound_module_pcm_usb_stream.so \
+/usr/lib/alsa-lib/libasound_module_pcm_vdownmix.so
 
 # Additional changes can be done in between here
 
