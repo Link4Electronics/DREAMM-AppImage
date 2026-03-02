f983ce0b34c379b3dbb59950d24f3307be384126
Link4Electronics <link4electronics@gmail.com>
2026-03-01 21:05:31 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,8 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+	alsa-lib \
+	alsa-utils \
     libdecor 	   \
     sdl2
 
